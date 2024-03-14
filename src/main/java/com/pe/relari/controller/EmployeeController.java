6136a36d43b77d86eb5be1d263f016df797690bf
Relari <renzolavador@gmail.com>
2024-03-14 14:40:48 -0500

    Migracion Java 17

--- src/main/java/com/pe/relari/controller/EmployeeController.java
+++ src/main/java/com/pe/relari/controller/EmployeeController.java
@@ -28,8 +28,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import java.util.stream.Collectors;
 
--- src/main/java/com/pe/relari/employee/model/api/EmployeeRequest.java
+++ src/main/java/com/pe/relari/employee/model/api/EmployeeRequest.java
@@ -2,10 +2,10 @@
 package com.pe.relari.employee.model.api;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
