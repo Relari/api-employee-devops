ae23f14bc6f892c7091748b30fa883f4fdfc6c1d
Relari <renzolavador@gmail.com>
2024-03-30 11:03:52 -0500

    updated exceptions

--- src/test/java/com/pe/relari/EmployeeApplicationTests.java
+++ src/test/java/com/pe/relari/EmployeeApplicationTests.java
@@ -1,98 +1,99 @@
-//package com.pe.relari;
-//
-//import com.pe.relari.employee.service.EmployeeService;
-//import com.pe.relari.employee.util.TestUtil;
-//import com.pe.relari.employee.util.GsonUtil;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-///**
-// * Class: EmployeeApplicationTests.
-// * @version 1.0.0
-// * @author Relari
-// */
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//class EmployeeApplicationTests {
-//
-//	@Value("${application.api.path}")
-//	private String baseUrl;
-//
-//	@Autowired
-//	private MockMvc mockMvc;
-//
-//	@Autowired
-//	private EmployeeService demoService;
-//
-//	@BeforeEach
-//	void init() {
-//		demoService.save(TestUtil.buildEmployee());
-//	}
-//
+package com.pe.relari;
+
+import com.pe.relari.employee.service.EmployeeService;
+import com.pe.relari.employee.util.TestUtil;
+import com.pe.relari.employee.util.GsonUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Class: EmployeeApplicationTests.
+ * @version 1.0.0
+ * @author Relari
+ */
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class EmployeeApplicationTests {
+
+	@Value("${application.api.path}")
+	private String baseUrl;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private EmployeeService demoService;
+
+	@BeforeEach
+	void init() {
+		demoService.save(TestUtil.buildEmployee());
+	}
+
+	@Test
+	void getDemosTest() throws Exception {
+		mockMvc.perform(get(baseUrl))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void createDemoTest() throws Exception {
+
+		var demo = TestUtil.buildEmployeeRequest();
+
+		mockMvc.perform(post(baseUrl)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(GsonUtil.toJson(demo))
+				)
+				.andDo(print())
+				.andExpect(status().isCreated());
+	}
+
 //	@Test
-//	void getDemosTest() throws Exception {
-//		mockMvc.perform(get(baseUrl))
-//				.andDo(print())
-//				.andExpect(status().isOk());
-//	}
+//	void createDemoButIsBadRequestTest() throws Exception {
 //
-//	@Test
-//	void createDemoTest() throws Exception {
-//
-//		var demo = TestUtil.buildEmployeeRequest();
+//		var demo = new DemoRequest("");
 //
 //		mockMvc.perform(post(baseUrl)
 //						.contentType(MediaType.APPLICATION_JSON)
-//						.content(GsonUtil.toJson(demo))
+//						.content(objectMapper.writeValueAsString(demo))
 //				)
 //				.andDo(print())
-//				.andExpect(status().isCreated());
+//				.andExpect(status().isBadRequest());
 //	}
 //
-////	@Test
-////	void createDemoButIsBadRequestTest() throws Exception {
-////
-////		var demo = new DemoRequest("");
-////
-////		mockMvc.perform(post(baseUrl)
-////						.contentType(MediaType.APPLICATION_JSON)
-////						.content(objectMapper.writeValueAsString(demo))
-////				)
-////				.andDo(print())
-////				.andExpect(status().isBadRequest());
-////	}
-////
-//	@Test
-//	void foundDemoTest() throws Exception {
-//		mockMvc.perform(get(baseUrl.concat("/1")))
-//				.andDo(print())
-//				.andExpect(status().isOk());
-//	}
-//
-//	@Test
-//	void demoNotFoundTest() throws Exception {
-//		mockMvc.perform(get(baseUrl.concat("/0")))
-//				.andDo(print())
-//				.andExpect(status().isNotFound());
-//	}
-//
-////	@Test
-////	void deleteDemoTest() throws Exception {
-////		mockMvc.perform(delete(baseUrl.concat("/1")))
-////				.andDo(print())
-////				.andExpect(status().isNoContent());
-////	}
-//
-//}
+	@Test
+	void foundDemoTest() throws Exception {
+		mockMvc.perform(get(baseUrl.concat("/1")))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void demoNotFoundTest() throws Exception {
+		mockMvc.perform(get(baseUrl.concat("/0")))
+				.andDo(print())
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	void deleteDemoTest() throws Exception {
+		mockMvc.perform(delete(baseUrl.concat("/1")))
+				.andDo(print())
+				.andExpect(status().isNoContent());
+	}
+
+}
